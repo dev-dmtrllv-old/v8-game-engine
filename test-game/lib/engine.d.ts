c612d2503b9a7246336375aca3ec5c5cfb79c38c
dev.dmtrllv <dev.dmtrllv@gmail.com>
2022-01-15 23:15:13 +0100

    added more documentation

--- test-game/lib/engine.d.ts
+++ test-game/lib/engine.d.ts
@@ -25,8 +25,21 @@ declare abstract class Scene
 {
 	public constructor();
 
+	/**
+	 * Here should all the assets that the current scene needs be loaded.
+	 * You can think of all the textures, audio files, shaders, material files etc...
+	 */
 	protected abstract load(): void;
+
+	/**
+	 * When the scene is fully loaded the call method will be called.
+	 * Here all the game objects are initialized with their coresponding components and data, so it's safe to access them.
+	 */
 	protected abstract start(): void;
+
+	/**
+	 * When another scene is loaded this scene's stop method will be called just before the other scene is started.
+	 */
 	protected abstract stop(): void;
 
 	/**
@@ -37,31 +50,67 @@ declare abstract class Scene
 	protected spawn(name: string, position: Vector): GameObject;
 }
 
-type SceneType<T extends Scene = Scene> = new (...args: any) => T;
+type SceneType<T extends Scene = Scene> = new () => T;
 
-declare var exports: any;
+declare var exports: { [key: string]: any; };
+
+declare class Exception extends Error
+{
+	public constructor(name: string, msg: string);
+
+	public toString();
+}
 
 declare namespace Engine
 {
+	class CoreException extends Exception
+	{
+		constructor(msg: string);
+	}
+
+	/**
+	 * This holds the main game window.
+	 */
 	const window: Window;
-	const onLoad: <T extends EngineConfiguration>(callback: OnLoadCallback<T>) => void;
+
+	/**
+	 * Call this to get an onLoadCallback.
+	 * The function is used to configure the engine with it's game specific needs.
+	 * @param onLoadCallback an async function to configure the engine
+	 */
+	const onLoad: (onLoadCallback: OnLoadCallback) => void;
+
+	/**
+	 * Log to the console
+	 * @param args list of objects to log (it can be anything).
+	 */
 	const log: (...args: any[]) => void;
+
+	/**
+	 * Call this method when the game is configured. If not the engine will throw an Engine.CoreException.
+	 * @param sceneName The name of the first scene to load.
+	 */
 	const start: (sceneName: string) => void;
+
+	/**
+	 * A method to retreive the current loaded (active) scene.
+	 * @returns Scene or null if no scene is loaded.
+	 */
 	const getActiveScene: () => Scene | null;
 }
 
-type OnLoadCallback<T extends EngineConfiguration> = (configure: EngineConfigureFunction<T>) => void;
+type OnLoadCallback = (configure: EngineConfigureFunction) => void;
 
-type EngineConfigureFunction<T extends EngineConfiguration> = (config: T) => Promise<void>;
+type EngineConfigureFunction = (config: EngineConfiguration) => Promise<void>;
 
 type EngineConfiguration = {
 	name: string;
 	window?: WindowConfig;
 	jobSystem?: JobSystemConfig;
+	graphics?: GraphicsConfig;
 	scenes?: {
 		[name: string]: SceneType
 	};
-
 };
 
 type Color = [number, number, number, number];
@@ -71,22 +120,55 @@ type GraphicsConfig = {
 };
 
 type JobSystemConfig = {
-	/** Total number of jobs to be executed */
+	/**
+	 * Total number of jobs to be executed.
+	*/
 	maxJobs?: number;
-	/** Total number of threads to execture the jobs. */
+	/** 
+	 * Total number of threads to execute the jobs. 
+	 * */
 	executionThreads?: number;
 };
 
 type WindowConfig = {
+	/**
+	 * The minimum with of the window.
+	 */
 	minWidth?: number;
+	/**
+	 * The minimum height of the window.
+	 */
 	minHeight?: number;
+	/**
+	 * The maximum with of the window.
+	 */
 	maxWidth?: number;
+	/**
+	 * The maximum height of the window.
+	 */
 	maxHeight?: number;
+	/**
+	 * The initial width of the window.
+	 */
 	width?: number;
+	/**
+	 * The initial height of the window.
+	 */
 	height?: number;
+	/**
+	 * Should the window be resizable?
+	 */
 	resizable?: boolean;
+	/**
+	 * Should the window start maximized?
+	 */
 	maximized?: boolean;
+	/**
+	 * Should the window start in fullscreen mode?
+	 */
 	fullscreen?: boolean;
+	/**
+	 * Should the window initially be hidden? 
+	 */
 	hidden?: boolean;
-	graphics?: GraphicsConfig;
 };
